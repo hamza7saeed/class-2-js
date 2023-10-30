223bc6dc94b409fce7006ab31f89840cc4f83274
hamza7saeed <126345476+hamza7saeed@users.noreply.github.com>
2023-10-30 16:55:10 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// Assignment For JS Class 2:
+
+// 1- Use All Three Ways To Run JavaScript Which We Discussed In Class 
+
+
+
+
+// 1st step to write javascript is internal.
+// 2nd step is extrnal.
+// 3rd step is to write on browser.
+// ..................
+// 1st
+// Internal..... we write internal javascript code in html file. just we need to
+// make file of html and start coding with <script> tag in html.
+// .....................
+// 2nd
+// Extrnal... In this concept, we just link our file with html file like just write script title in 
+// body section along with "src=""" attribute and inside in the cotitation marks we need 
+// to add javascript name and start coding in javascript file.
+document.write("Hello world");
+// .................
+// 3rd
+// In this 3rd section, we need to write our code in web browser.
+
+
+
+// ,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+// 2- Show The Concepts Of Scope By Using In Your Code 
+// Scope is the activity in which we have to access the code. There are 2 types of scope
+// (1). Global scope (2). Local scope
+// Global scope..... In this scope type, we access our code globally whereas 
+// in Local scope... we have limitation like olny we access those block of codes which are written only in  blocks 
+// ,,,,.,...............
+// 3- Describe JavaScript Engine
+// It is an engine who support javascript code for running in browser. Its like an environment who assist 
+// our javascript code..
+// 
+// 
+// 
+// 
